4d2e7cb3e7334ee574542e55c8dd73270791fb6a
john <1593860024@qq.com>
2017-07-30 22:09:22 +0800

    登录的前台与后台测试完成

--- e-teacher/src/main/java/com/xulifei/e/controller/AccountAction.java
+++ e-teacher/src/main/java/com/xulifei/e/controller/AccountAction.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/account")
@@ -17,27 +19,37 @@ public class AccountAction extends BaseAction {
     @Resource
     private AccountService accountService;
 
-    @RequestMapping(value="/login")
-    public String login(User user, HttpServletRequest request, HttpSession session){
+    @RequestMapping(value="/login",produces="application/json;charset=UTF-8")
+    @ResponseBody
+    public Object login(User user, HttpServletRequest request, HttpSession session) {
+
+        Map  map=  new HashMap<String,User>();
         System.out.println("---action.account:"+user);
+        System.out.println("账号"+user.getAccount()+"密码"+user.getPwd());
         User acc= accountService.login(user);
         if(acc!=null){
             //存session
             session.setAttribute("user", acc);
-            return "forward:/WEB-INF/main.jsp";
-        }else{
-            request.setAttribute("msg", "用户名或密码错误！");
-            return "redirect:/login.jsp";
+            map.put("isLogin",true);
+            map.put("user",acc);
+            return map;
         }
+
+       map.put("isLogin",false);
+        map.put("user",acc);
+            return map;
 
 
     }
-
-    @RequestMapping("/doAjax")
+    //produces="text/html;charset=UTF-8",避免返回的中文数据乱码,设置的是reponse的Content-Type:application/json;charset=UTF-8即返回的数据格式
+    @RequestMapping(value="/doAjax",produces="application/json;charset=UTF-8")
     @ResponseBody //如果返回json格式，需要这个注解，这里用来测试环境
     public Object doAjax(User user){
         System.out.println("---doAjax.user:"+user);
-        user.setUserName(user.getUserName());
+        System.out.println(user.getAccount());
+        System.out.println(user.getPwd());
+        user.setUserName("xulifei");
+//        System.out.println(account+"   "+pwd);
         return user;
     }
 
--- e-teacher/src/main/webapp/login.jsp
+++ e-teacher/src/main/webapp/login.jsp
@@ -11,7 +11,7 @@
     <title>Title</title>
 </head>
 <body>
-<form action="${pageContext.request.contextPath}/account/login.action" method="post" >
+<form action="${pageContext.request.contextPath}/account/doAjax.action" method="post" >
 账号：<input type="text" name="account"><br>
 密码：<input type="password" name="pwd">
 <input type="submit" value="提交">
